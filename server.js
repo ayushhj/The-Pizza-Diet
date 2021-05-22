74d99d7aa40f6224d9ae94ebd9ab7f028b2c512a
ayushhj <ayushjoshi494@gmail.com>
2021-05-23 00:43:19 +0530

    auth system

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDBStore = require('connect-mongo')
+const passport = require('passport')
 
 
 //Database connection
@@ -21,8 +22,6 @@ connection.once('open',()=>{
 });
 
 const PORT = process.env.PORT || 3000
-
-
 
 //Session store
 // let mongoStore = new MongoDBStore({
@@ -42,13 +41,27 @@ app.use(session({
 
 }))
 
+//Passport config
+const passportInit = require('./app/config/passport')
+//const { Passport } = require('passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
+
+
+
 app.use(flash())
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 
 //Global middleware
 app.use((req,res,next)=>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 
 })
